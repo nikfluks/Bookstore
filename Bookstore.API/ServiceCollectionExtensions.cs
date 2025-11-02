c3cba98f58686f360f213315d6652f896090d526
Nikola Fluks <nikola.fluks@gmail.com>
2025-11-02 12:16:10 +0100

    Swagger enabled

---
+++ Bookstore.API/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Bookstore.API
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddOpenApi("bookstore");
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new() { Title = "Bookstore API", Version = "v1" });
+            });
+
+            return services;
+        }
+    }
+}
